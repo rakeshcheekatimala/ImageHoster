616aca6ed44022ea2447a916bcbea19e2c76a182
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2020-01-13 21:43:19 +0800

    adding changes related to comments to the model

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -1,8 +1,10 @@
 package ImageHoster.controller;
 
+import ImageHoster.model.Comment;
 import ImageHoster.model.Image;
 import ImageHoster.model.Tag;
 import ImageHoster.model.User;
+import ImageHoster.service.CommentService;
 import ImageHoster.service.ImageService;
 import ImageHoster.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,9 @@ public class ImageController {
 
     @Autowired
     private TagService tagService;
+
+    @Autowired
+    private CommentService commentService;
 
     //This method displays all the images in the user home page after successful login
     @RequestMapping("images")
@@ -67,6 +72,10 @@ public class ImageController {
         Image image = imageService.getImage(id);
         model.addAttribute("image", image);
         model.addAttribute("tags", image.getTags());
+        List<Comment> comments = commentService.getAllComments(image.getId());
+        model.addAttribute("comments", comments);
+        System.out.println("Inside showImage");
+        System.out.println(model);
         return "images/image";
     }
 
